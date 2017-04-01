02f89a9a5987093c756cc321c898449a33804058
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-01 08:19:02 +0100

    Set the user-agent as Mozilla

--- scraper.py
+++ scraper.py
@@ -40,6 +40,8 @@ def scrape_table(root):
 
 #create our browser
 br = mechanize.Browser()
+# Set the user-agent as Mozilla - if the page knows we're Mechanize, it won't return all fields
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 #store the results of opening the URL with that browser
 response = br.open(lotterygrantsurl)
 
